03044f2b34b8d7801320b339cbd6742fd42bf650
TomaAghakhanyan <110786458+TamaraAghakhanyan@users.noreply.github.com>
2022-08-09 22:44:14 +0400

    'First'

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Hello World!");
+             /*Console.WriteLine("I am learning C#");
+             Console.WriteLine("It is awesome");
+             Console.WriteLine("3 + 3");*/
+             //Console.Write("Hello Hello");
+            string name = "Toma";
+            Console.WriteLine(name);
+           /*int myNum = 27;
+            Console.WriteLine(myNum);*/
+            int myNum = -123;
+            myNum = 200; //the value is changed
+            Console.WriteLine(myNum);
+            double pointNum = 12.3;
+            Console.WriteLine(pointNum);
+            char capLetter = 'A';
+            Console.WriteLine(capLetter);
+            char notCapLetter = 'd';
+            Console.WriteLine(notCapLetter);
+            string myName = "Tamara";
+            Console.WriteLine(myName);
+            string surName = " Aghakhanyan";
+            Console.WriteLine(surName);
+            bool TheWeatherIsNice = true;
+            Console.WriteLine(TheWeatherIsNice);
+            bool TheWeatherIsCloudy = false;
+            Console.WriteLine(TheWeatherIsCloudy);
+            int myNumber;
+            myNumber = 28;
+            Console.WriteLine(myNumber);
+            const int myAge = 27;
+            Console.WriteLine(myAge);
+            string fullName = myName + surName;
+            Console.WriteLine(fullName);
+            Console.WriteLine(myNum + myAge);
+            string space = " ";
+            Console.WriteLine(space);
+            string numbers = "2" + "00";
+            Console.WriteLine("2" + "00");
+            string number1 = "077";
+            Console.WriteLine(number1);
+            string number2 = "600";
+            Console.WriteLine(number2);
+            string number3 = "961";
+            Console.WriteLine(number3);
+            string phoneNumber = number1 + space + number2 + space + number3;
+            Console.WriteLine(phoneNumber);
+            int number4 = 100;
+            Console.WriteLine(number4);
+            int number5 = 200;
+            Console.WriteLine(number5);
+            int number6 = 500;
+            Console.WriteLine(number6);
+            Console.WriteLine(number4 + number5 + number6);
+            int x, y, z;
+            x = y = z = 1000;
+            Console.WriteLine(x + y + z);
+            long longNumber = 39848853453549L;
+            Console.WriteLine(longNumber);
+
+
+
+            
+        }
+    }
+}
